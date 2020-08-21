d874c886a46ad9ff9d2d52fd082fbb65e78d2796
hong yang <you@example.com>
2020-08-21 21:32:44 +0800

    重新打包

---
+++ build/precache-manifest.eaf44a24ed5cb0c181c5ac55b95df063.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad566c56914e7b1a340349193fa449e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e221e2cbb576aa16354",
+    "url": "/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "fc84059074ec2a591a5e",
+    "url": "/static/css/main.46450c16.chunk.css"
+  },
+  {
+    "revision": "4e221e2cbb576aa16354",
+    "url": "/static/js/2.bdb4f9a1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bdb4f9a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc84059074ec2a591a5e",
+    "url": "/static/js/main.e12024f5.chunk.js"
+  },
+  {
+    "revision": "75c418ba89a9c83df653",
+    "url": "/static/js/runtime-main.c51847dd.js"
+  }
+]);
